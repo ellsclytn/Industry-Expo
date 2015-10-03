6d6d9f8a8192bff7dba4da24099b798b89a548c9
Jack Cuthbert <jackcuthbert1@gmail.com>
2015-10-03 11:07:45 +1000

    updated readme, added task to copy bolt config, added bower install to postinstall script

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,19 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // Copying files to places!
+    copy: {
+      // NOTE: This task will only work if you run grunt from inside the theme directory which should be under
+      //       /path/to/Bolt/theme/industry-expo.
+      boltConfig: {
+        expand: true,
+        cwd: 'bolt-config/',
+        src: '*.yml',
+        dest: '../../app/config/',
+      },
+    },
+
+    // Compile Sass with compass into css/ directory
     compass: {
       dist: {
         options: {
@@ -12,16 +25,22 @@ module.exports = function(grunt) {
       },
     },
 
+    // Watch files that change frequently and require processing.
     watch: {
       compass: {
         files: 'scss/**/*.scss',
         tasks: ['compass']
+      },
+      boltMenuSetup: {
+        files: 'bolt-config/*.yml',
+        tasks: ['copy:boltConfig']
       }
     },
 
+    // Browser Sync task to automatically refresh the page on any changes that affect appearance.
     browserSync: {
       bsFiles: {
-        src: ['css/*.css', '*.twig']
+        src: ['css/*.css', '*.twig', '../../app/config/*.yml']
       },
       options: {
         watchTask: true,
@@ -31,11 +50,12 @@ module.exports = function(grunt) {
   });
 
   // Load Tasks
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Register Tasks
-  grunt.registerTask('build', ['compass']);
+  grunt.registerTask('build', ['compass', 'copy']);
   grunt.registerTask('default', ['build', 'browserSync', 'watch']);
 }
--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-prExpo
-=========
+# Industry Expo Bolt CMS Theme
 
-This is the Industry Project Expo (prExpo) theme, based on the ["base-2014"](https://github.com/bolt/base-2014) theme for Bolt. It is built with Foundation 5 and Sass, but you can use it
-as-is.
+This is the Industry Project Expo theme for [Bolt CMS](https://bolt.cm), based on the ["base-2014"](https://github.com/bolt/base-2014) theme for Bolt. It is built with [Pure CSS](http://purecss.io) and [Compass](http://compass-style.org).
 
-To install manually, go to `theme/` and do a checkout: 
+## Requirements
 
-  git clone https://github.com/ellsclytn/Industry-Expo.git
+* [Node.js](https://nodejs.org/en/) and [Ruby](https://www.ruby-lang.org/en/) installed.
+* Grunt and bower: `npm install bower grunt-cli -g`
+* Compass: `gem install compass`
 
-Set `theme: prExpo` in your `app/config/config.yml`, and you're good to go! 
+## Installation
 
-If you wish to modify this theme using Sass, be sure to install the 'Foundation'
-bower module, and start your favorite Sass parser, for example 'grunt':
-
-```
-bower install
-npm install
-grunt watch
-```
+1. Change to Bolt CMS theme/ dir: `cd /path/to/Bolt/theme`
+2. Clone repo: `git clone https://github.com/ellsclytn/Industry-Expo.git 'industry-expo'`
+3. Move to theme dir `cd industry-expo`
+4. Install dependencies: `npm install`
+    * This runs `bower install` for you!
+5. Set `theme: industry-expo` in `app/config/config.yml`
+6. Start grunt: `grunt`
