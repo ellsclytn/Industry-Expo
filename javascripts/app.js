ccad7a2c5a92992c9b85dbb3ea5d351f55bf5e44
Jack Cuthbert <jackcuthbert1@gmail.com>
2015-10-06 21:48:51 +1000

    converted masthead to flexbox, added template tags

---
+++ javascripts/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  // Masthead "what's this about?" button
+  $('.flex-item.button').click(function() {
+    // TODO: Scroll to section
+    
+    return false;
+  });
+
+});
