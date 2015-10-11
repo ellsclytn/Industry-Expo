0d20bfecb05c6c7f3afb6c0414fb9249d1c66518
Ellis Clayton <ellis@ellisclayton.info>
2015-10-11 20:34:03 +1000

    Google map canvas

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,8 +3,27 @@ $(document).ready(function() {
   // Masthead "what's this about?" button
   $('.flex-item.button').click(function() {
     // TODO: Scroll to section
-    
+
     return false;
   });
 
 });
+
+var map;
+function initMap() {
+  if ($('#map-canvas').length) {
+    var location = new google.maps.LatLng(-27.5538582,153.0546691);
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+      position: location,
+      title: 'Red Zone'
+    });
+
+    marker.setMap(map);
+  }
+}
